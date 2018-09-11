039744c65b899b4ba3dfcd14e5a4091c59242221
Tim Fuller <tim.fuller@palantirsolutions.com>
2018-09-11 09:35:12 +0100

    Vanilla Food Controller

---
+++ GrupsUp/GrupsUp/ApiController/FoodController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper.Execution;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace GrupsUp.ApiController
+{
+    [Route("api/foods")]
+    public class FoodController : Controller
+    {
+
+
+        public FoodController()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [FormatFilter]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("You Must be hungry.");
+        }
+
+        /// <summary>
+        /// Describe the food model object
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>
+        /// Describes Stuff
+        /// </remarks>
+        [HttpGet]
+        [Route("describe")]
+
+        public IActionResult Describe()
+        {
+            return Ok("HelloWorld");
+        }
+    }
+}
